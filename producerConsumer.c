b935654e8d1714af32cec415d93b22a18f5493bb
NikhilReddy-2 <nikhil@kawa.space>
2022-01-08 11:40:33 +0530

    Basic stubs for functions

---
+++ producerConsumer.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+
+#define MAX 100000
+
+
+void * produce(void * args){
+
+    return NULL;
+}
+
+void * consume(void * args){
+
+    return NULL;
+}
+
+int main(int argc, char * argv){
+
+
+
+    
+    return 0;
+}
